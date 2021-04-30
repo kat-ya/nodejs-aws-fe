23748b77be210a146c02dde5f8dde38129479222
Ekaterina Kazantseva <ekaterina.kazantseva@non.se.com>
2021-04-30 19:25:12 +0300

    feat: update import API

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   products: 'https://a0gy9b4tbj.execute-api.us-east-1.amazonaws.com/dev/products',
   product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://vzv3uqe62c.execute-api.us-east-1.amazonaws.com/dev/import',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
