bb4ca343a61a3d9751e08905e30dc44c3d20edea
Ekaterina Kazantseva <ekaterina.kazantseva@non.se.com>
2021-07-04 20:16:50 +0300

    feat: update api endpoint for bff

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,10 +1,10 @@
 
 const API_PATHS = {
   products: 'https://a0gy9b4tbj.execute-api.us-east-1.amazonaws.com/dev/products',
-  product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  product: 'https://a0gy9b4tbj.execute-api.us-east-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://vzv3uqe62c.execute-api.us-east-1.amazonaws.com/dev/import',
-  bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  bff: 'https://kat-ya-bff-api-develop.us-east-1.elasticbeanstalk.com/',
   cart: 'https://kat-ya-cart-api-dev.us-east-1.elasticbeanstalk.com/api',
 };
 
